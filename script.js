6119dc26f3e1d632729950514ea0f3e400a70b6c
Flaviojr_dev <flaviobarbosabsjt@gmail.com>
2025-09-01 09:20:54 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const hamburger = document.getElementById("hamburger");
+const menuMobile = document.getElementById("menu-mobile");
+
+hamburger.addEventListener("click", () => {
+    menuMobile.classList.toggle("active");
+});
